ae3c5a7b9c1e7271e4109839b612042a66236909
Christian <c_pfeiffer325@yahoo.ca>
2019-05-02 18:45:42 +0000

    added printing waldos index in the array

--- find-waldo.js
+++ find-waldo.js
@@ -1,14 +1,15 @@
 // The second argument/parameter is expected to be a function
+// The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
   for (var i = 0; i < arr.length; i++) {
     if (arr[i] === "Waldo") {
-      found();   // execute callback
+      found(i);   // execute callback
     }
   }
 }
 
-function actionWhenFound() {
-  console.log(`Found Waldo at index ${found}`);
+function actionWhenFound(waldoIndex) {
+  console.log(`Found Waldo at index ${waldoIndex}`);
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
