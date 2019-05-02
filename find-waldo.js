a9ebc2cccd048c98c5f5e292a8f2c8485a378bcd
Christian <c_pfeiffer325@yahoo.ca>
2019-05-02 19:11:12 +0000

    added the for each method to finding waldo

--- find-waldo.js
+++ find-waldo.js
@@ -1,11 +1,11 @@
 // The second argument/parameter is expected to be a function
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(function(waldo, index) {
+    if (waldo === "Waldo") {
+      found(index);   // execute callback
     }
-  }
+  });
 }
 
 function actionWhenFound(waldoIndex) {
